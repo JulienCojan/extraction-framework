e16fe16f23f24015ebca306800674bb0bae3cfa6
JulienCojan <julien.cojan@amadeus.com>
2014-06-29 21:25:04 +0200

    added mappings extractor with auto download of the onlotology + mappings

--- run_extraction_fr.sh
+++ run_extraction_fr.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-BASE_DIR='/data/dbpedia-release/data'
+BASE_DIR='/data/dbpedia-release/data' #idem 'base-dir' in extraction.properties
 LANG='fr'
 
 INITIAL_PATH=$(realpath ${0%/*})
+
+## download ontology + mappings
+cd core
+../run download-ontology
+../run download-mappings
+cd ${INITIAL_PATH}
+
+## run the extraction
 cd dump
-
 ../run download "config=download.properties"
-
+../run import
 ../run extraction "extraction.wikipedia-fr.properties"
 ../run extraction "extraction.wikidata.properties"
 
